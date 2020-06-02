81ea5ea3ae438bd46bac0d3d1f77c033b83bfb2c
sbensaoud <65964124+sbensaoud@users.noreply.github.com>
2020-06-02 14:34:45 +0100

    update

--- MovieZtream/mz/startup.sh
+++ MovieZtream/mz/startup.sh
@@ -3,15 +3,15 @@ CWD=${PWD}
 
 
 if [ -n "${ui}" ]; then
-        cp /apps/movieztream_ui.war /tomcat/webapps;
+        cp /apps/movieztream_ui.war /usr/local/tomcat/webapps/;
 fi
 
 if [ -n "${sv}" ]; then
- 	cp /apps/movieztream_service.war /tomcat/webapps;
+ 	cp /apps/movieztream_service.war /usr/local/tomcat/webapps/;
 fi
 
 if [ -n "${rt}" ]; then
-        cp /apps/movieztream_rating.war /tomcat/webapps;
+        cp /apps/movieztream_rating.war /usr/local/tomcat/webapps/;
 fi
 
 cd ${CWD}
